44f3e8dd40b25d07589a2ec2d500dfa2e0179bf3
Marcosmartin16 <sr_eme@outlook.es>
2022-10-10 18:32:10 +0200

    mongo

--- EjerciciosClassroom/Conversion/Conversion.js
+++ EjerciciosClassroom/Conversion/Conversion.js
@@ -4,29 +4,32 @@ let factor = ""
     let numeros = [];
     let conversor = [];
 
-    for(let i = 1; i <= 10; i++){
-        let numero = prompt("introduce el numero " + i);
-        numeros.push(numero);
-    }
-
-    document.getElementById("respuesta1").innerHTML = "valores de entrada en " + factor + " " + numeros;
-
-    if(factor == "C"){
-        for(let i = 1; i<= 10; i++){
-            factor = "F";
-            let numero_final = (numeros[i-1] - 32) * 5/9;
-            conversor.push(numero_final);
+    if(factor.ignoreCase != "salir"){
+        for(let i = 1; i <= 10; i++){
+            let numero = prompt("introduce el numero " + i);
+            numeros.push(numero);
+        }
+    
+        if(factor == "C"){
+            document.getElementById("respuesta1").innerHTML = "valores de entrada en " + factor + " " + numeros;
+            for(let i = 1; i<= 10; i++){
+                factor = "F";
+                let numero_final = (numeros[i-1] - 32) * 5/9;
+                conversor.push(numero_final);
+            }
+            document.getElementById("respuesta2").innerHTML = "valores de salida en " + factor + " " + conversor;
+        }else{
+            document.getElementById("respuesta1").innerHTML = "valores de entrada en " + factor + " " + numeros;
+            for(let i = 1; i<= 10; i++){
+                factor = "C";
+                let numero_final = (numero * 9/5) + 32;
+                conversor.push(numero_final);
+            }
+            document.getElementById("respuesta2").innerHTML = "valores de salida en " + factor + " " + conversor;
         }
     }else{
-        for(let i = 1; i<= 10; i++){
-            factor = "C";
-            let numero_final = (numero * 9/5) + 32;
-            conversor.push(numero_final);
-        }
+        document.getElementById("respuesta1").innerHTML = "Usted eligio salir";
     }
-
-
-    document.getElementById("respuesta2").innerHTML = "valores de salida en " + factor + " " + conversor;
  }
 
 
