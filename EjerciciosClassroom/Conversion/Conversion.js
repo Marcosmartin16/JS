82ae515454c7a2d127657489b54ab33c025d98d9
Marcosmartin16 <sr_eme@outlook.es>
2022-10-10 17:54:46 +0200

    conversor pocho

--- EjerciciosClassroom/Conversion/Conversion.js
+++ EjerciciosClassroom/Conversion/Conversion.js
@@ -1,21 +1,32 @@
-window.onload = () => {
-    
-}
-function capturar(){
-    let spinner = document.getElementById("spinner");
 
-    let indice = document.spinner.selectedIndex;
 
-    let valor = document.spinner.options[indice].value;
+    let factor = prompt("C para Celsisus o F para Fahrenheit");
 
-    if(valor == "Celsius"){
-        let numero = document.getElementById("numero").value;
-        let numero_final = (numero - 32) * 5/9;
-        document.write(numero_final); 
-    }else{
-        let numero = document.getElementById("numero").value;
-        let numero_final = (numero * 9/5) + 32;
-        document.write(numero_final);
+    let numeros = [];
+    let conversor = [];
+
+    for(let i = 1; i <= 10; i++){
+        let numero = prompt("introduce el numero " + i);
+        numeros.push(numero);
     }
-}
+
+    document.getElementById("respuesta1").innerHTML = "valores de entrada en " + factor + " " + numeros;
+
+    if(factor == "C"){
+        for(let i = 1; i<= 10; i++){
+            factor = "F";
+            let numero_final = (numeros[i-1] - 32) * 5/9;
+            conversor.push(numero_final);
+        }
+    }else{
+        for(let i = 1; i<= 10; i++){
+            factor = "C";
+            let numero_final = (numero * 9/5) + 32;
+            conversor.push(numero_final);
+        }
+    }
+
+
+    document.getElementById("respuesta2").innerHTML = "valores de salida en " + factor + " " + conversor;
+
 
