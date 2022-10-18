d7f69d04c5d114e2d968d28a9dd74967d56046b1
Marcosmartin16 <sr_eme@outlook.es>
2022-10-18 20:55:14 +0200

    js

--- EjerciciosU3/AlumnosMedia/Media.js
+++ EjerciciosU3/AlumnosMedia/Media.js
@@ -1,25 +1,47 @@
-let alumnos = parseFloat(prompt("Introduce el numero de alumnos menos de 20 y mas de 8"));
+let alumnos = parseFloat(prompt("Introduce el numero de alumnos (menos de 20 y mas de 8)"));
 let array = [];
 let notas = [];
-let nombre = [];
+let nombres = [];
+let total = 0;
+
+function sumar(a){
+    total += a;
+}
 
 for (let i = 0; i < alumnos; i++) {
     let nombre = prompt("introduce nombre del alumno");
-    let nota = prompt("introduce la nota del alumno");
+    let nota = parseFloat(prompt("introduce la nota del alumno"));
 
-    array.push(nombre, nota);
+    notas.push(nota);
+    nombres.push(nombre);
 }
 
-/*for (let j = 0; j < array.length; j++){
+var body = document.getElementById("body");
+var tabla = document.getElementById("tabla");
+var tbody = document.getElementById("tbody");
 
-    if(i % 2 != 0){
-        notas.push[array[j]];
-    }else{
-        nombre.push[array[j]];
+for (var i = 0; i < 2; i++){
+    var hilera = document.createElement("tr");
+
+    for (var j = 0; j < 2; j++){
+        var celda = document.createElement("td");
+        var texto = document.createTextNode("celda " + i + " columna " + j);
+        celda.appendChild(texto);
+        hilera.appendChild(celda);
     }
-}*/
 
-document.getElementById("media").innerHTML = array + array.length;
+    tbody.appendChild(hilera);
+}
+
+tabla.appendChild(tbody);
+body.appendChild(tabla);
+tabla.setAttribute("border","2");
+
+notas.forEach(sumar);
+
+let media = total / alumnos;
+
+document.getElementById("media").innerHTML = "La media de las notas de los alumnos introducidas es de: " + media;
 
 
 
